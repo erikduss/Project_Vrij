651f94705ea55e747a29fe0de0b9c65d3d76e7d8
Erikduss <erik.dusschooten@student.hku.nl>
2020-05-31 14:21:49 +0200

    Implemented voice lines/story

---
+++ Unity_Project/Project_Vrij/Assets/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private SoundManager soundManager;
+    public int storyProgress = 0;
+
+    public GameObject fadePanel;
+    private Image panelImage;
+
+    private bool ending = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundManager = this.gameObject.GetComponent<SoundManager>();
+        storyProgress = -1;
+        panelImage = fadePanel.GetComponent<Image>();
+        fadePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(storyProgress < 0)
+        {
+            storyProgress++;
+            //soundManager.IntroVoiceLinesLevel1();
+        }
+
+        if (ending)
+        {
+            panelImage.CrossFadeAlpha(1, 2.5f, false);
+        }
+    }
+
+    public void EndDemo()
+    {
+        fadePanel.SetActive(true);
+        Color fixedColor = panelImage.color;
+        fixedColor.a = 1;
+        panelImage.color = fixedColor;
+        panelImage.CrossFadeAlpha(0, 0.0f, false);
+
+        soundManager.VoiceLinesLevel1End();
+        ending = true;
+    }
+
+    public void NextStoryVoiceLines()
+    {
+        storyProgress++;
+        switch (storyProgress)
+        {
+            case 1:
+                soundManager.VoiceLinesLevel1Part1();
+                break;
+            case 2:
+                soundManager.VoiceLinesLevel1Part2();
+                break;
+            case 3:
+                soundManager.VoiceLinesLevel1Part3();
+                break;
+            case 4:
+                soundManager.VoiceLinesLevel1Part4();
+                break;
+        }
+    }
+}
---
+++ Unity_Project/Project_Vrij/Assets/SoundManager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public List<AudioClip> Level_1_Voice_Lines = new List<AudioClip>();
+    private int currentVoiceLine = 0;
+    private bool allowNextVoiceLine = false;
+    public AudioSource nerrativeAudio;
+
+    public List<GameObject> nerrativeBlockers = new List<GameObject>();
+
+    public List<GameObject> nerrative3DPositions = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentVoiceLine = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IntroVoiceLinesLevel1()
+    {
+        nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+        currentVoiceLine++;
+    }
+
+    #region Level 1 Part 1
+    public void VoiceLinesLevel1Part1()
+    {
+        nerrativeAudio.transform.position = nerrative3DPositions[0].transform.position;
+        nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+        currentVoiceLine++;
+        StartCoroutine(voiceLineLevel1Delay(6,false));
+    }
+
+    private IEnumerator deactivateBlocker(int blocker)
+    {
+        yield return new WaitForSeconds(2);
+        if (nerrativeBlockers[blocker].activeInHierarchy == true)
+        {
+            nerrativeBlockers[blocker].SetActive(false);
+        }
+    }
+
+        private IEnumerator voiceLineLevel1Delay(int delay, bool part4)
+    {
+        yield return new WaitForSeconds(delay);
+        nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+        currentVoiceLine++;
+        StartCoroutine(deactivateBlocker(0));
+        if (part4)
+        {
+            yield return new WaitForSeconds(7);
+            nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+            currentVoiceLine++;
+            yield return new WaitForSeconds(3);
+            nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+            currentVoiceLine++;
+            yield return new WaitForSeconds(3);
+            nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+            currentVoiceLine++;
+            yield return new WaitForSeconds(4);
+            nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+            currentVoiceLine++;
+        }
+    }
+    #endregion
+
+    public void VoiceLinesLevel1Part2()
+    {
+        nerrativeAudio.transform.position = nerrative3DPositions[0].transform.position;
+        nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+        currentVoiceLine++;
+        StartCoroutine(deactivateBlocker(1));
+    }
+
+    public void VoiceLinesLevel1Part3()
+    {
+        nerrativeAudio.transform.position = nerrative3DPositions[1].transform.position;
+        nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+        currentVoiceLine++;
+        StartCoroutine(deactivateBlocker(2));
+    }
+
+    public void VoiceLinesLevel1Part4()
+    {
+        nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[currentVoiceLine]);
+        currentVoiceLine++;
+        StartCoroutine(voiceLineLevel1Delay(5,true));
+    }
+
+    public void VoiceLinesLevel1End()
+    {
+        nerrativeAudio.PlayOneShot(Level_1_Voice_Lines[0]);
+    }
+
+}
