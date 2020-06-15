9283013db1d3da175b531eaa5cc317c5fe2d997d
NowiFate <themadkinggangrel@gmail.com>
2020-06-15 12:09:02 +0200

    Full Commitment's what I'm going for

---
+++ Unity_Project/Project_Vrij/Assets/Nowi Prefabs/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 3.5f;
+    public float jumpHeight = 30f;
+
+    private Rigidbody rb;
+    public GameObject groundCheck;
+
+    private void Awake()
+    {
+        rb = GetComponentInChildren<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Move
+        float vert = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+
+        Vector3 moveDir = vert * transform.forward + hor * transform.right;
+
+        rb.MovePosition(transform.position + moveDir.normalized * Time.deltaTime * moveSpeed);
+
+        //Jump
+        RaycastHit hit;
+        if (Physics.Raycast(groundCheck.transform.position, -Vector3.up, out hit, 1f))
+        {
+
+            if (Input.GetButtonDown("Jump"))
+            {
+
+                rb.AddForce(Vector3.up * jumpHeight);
+            }
+        }
+    }
+}
