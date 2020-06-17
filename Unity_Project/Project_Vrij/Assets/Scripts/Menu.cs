b64f0988bf3b4e3b807f020b552c28cd425de240
Jelmer de Voogt <jelmer@robotkittens.nl>
2020-06-17 16:06:52 +0200

    Menu behaviour + pulse effect gems

--- Unity_Project/Project_Vrij/Assets/Scripts/Menu.cs
+++ Unity_Project/Project_Vrij/Assets/Scripts/Menu.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
-{
+{ 
+[SerializeField]private Image fadePanel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +19,28 @@ public class Menu : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-
+            StartCoroutine(OnGameStart());
         }
 
     }
+
+    private IEnumerator OnGameStart()
+    {
+        StartCoroutine(FadePanel());
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Grot_1");
+    }
+
+    private IEnumerator FadePanel() {
+
+        for (float i = 0; i <= 1; i += Time.deltaTime)
+        {
+
+            fadePanel.color = new Color(0, 0, 0, i);
+            yield return null;
+        }
+    }
+
+
+
 }
