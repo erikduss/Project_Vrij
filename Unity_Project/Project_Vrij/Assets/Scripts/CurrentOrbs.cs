5e76ed70ad84ef56c05af738933a56db885a08db
knuffelturk <aran1997.ay@gmail.com>
2020-06-17 21:30:29 +0200

    Scriptjes begin

---
+++ Unity_Project/Project_Vrij/Assets/Scripts/CurrentOrbs.cs
@@ -0,0 +1,38 @@
+﻿public static class CurrentOrbs
+{ 
+    //private static Orb orb1, orb2, orb3;
+
+    public static Orb orb1
+    {
+        get
+        {
+            return orb1;
+        }   
+        set
+        {
+            orb1 = value;
+        }
+    }
+    public static Orb orb2
+    {
+        get
+        {
+            return orb2;
+        }
+        set
+        {
+            orb2 = value;
+        }
+    }
+    public static Orb orb3
+    {
+        get
+        {
+            return orb3;
+        }
+        set
+        {
+            orb3 = value;
+        }
+    }
+}
---
+++ Unity_Project/Project_Vrij/Assets/Scripts/InventorySystemAran.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystemAran : MonoBehaviour
+{
+
+    public Orb orbGrot1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "orb" && Input.GetKeyDown("e"))
+        {
+            CurrentOrbs.orb1 = orbGrot1;
+        }
+    }
+}
