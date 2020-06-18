6bcd7a95f15119ff70038ba32a18033e98e514c7
NowiFate <themadkinggangrel@gmail.com>
2020-06-18 09:45:37 +0200

    FINAL COMMIT

---
+++ Unity_Project/Project_Vrij/Assets/Nowi Prefabs/G10.Crys/Colourchange.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Colourchange : MonoBehaviour
+{
+    public Animator lightSwitch;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))lightSwitch.enabled = true;
+    }
+}
